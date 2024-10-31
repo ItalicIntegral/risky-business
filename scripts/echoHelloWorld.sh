3b35c273bf27b6403bab32ee6bdb6f445d119294
ItalicIntegral <benjolin@protonmail.com>
2024-10-31 01:50:28 -0600

    Add echoHelloWorld.sh

---
+++ scripts/echoHelloWorld.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# does this script have execute permission?
+# ls -l
+# https://www.howtouselinux.com/post/check-file-permissions-in-linux
+
+# add execute permission to this script. 
+# chmod +x ./echoHelloWorld.sh
+
+# execute this script
+# bash ./echoHelloWorld
+# don't run or could trigger an infinite recursive loop?
+
+echo helloWorld
